1078c7a9330cb557de78d15a4dee3f18f8f8c030
ahamedmuiz119@gmail.com <ahamedmuiz119@gmail.com>
2025-09-13 00:27:37 +0530

    init commit

---
+++ src/main/java/com/driving_school_hibernate/controller/ProfilePageController.java
@@ -0,0 +1,45 @@
+package com.driving_school_hibernate.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class ProfilePageController {
+
+    @FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnUpdateProfile;
+
+    @FXML
+    private PasswordField txtConfirmPassword;
+
+    @FXML
+    private TextField txtEmail;
+
+    @FXML
+    private PasswordField txtPassword;
+
+    @FXML
+    private TextField txtRole;
+
+    @FXML
+    private TextField txtUserId;
+
+    @FXML
+    private TextField txtUsername;
+
+    @FXML
+    void handleClearForm(ActionEvent event) {
+
+    }
+
+    @FXML
+    void handleUpdateProfile(ActionEvent event) {
+
+    }
+
+}
