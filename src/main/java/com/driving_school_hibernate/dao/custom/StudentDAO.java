7a7dde8fe00279a67aa35fb331e6477f7b967e7e
ahamedmuiz119@gmail.com <ahamedmuiz119@gmail.com>
2025-09-15 00:44:01 +0530

    init commit

--- src/main/java/com/driving_school_hibernate/dao/custom/StudentDAO.java
+++ src/main/java/com/driving_school_hibernate/dao/custom/StudentDAO.java
@@ -3,7 +3,7 @@ package com.driving_school_hibernate.dao.custom;
 import com.driving_school_hibernate.dao.CrudDAO;
 import com.driving_school_hibernate.entity.StudentEntity;
 
-public interface StudentDAO extends CrudDAO<StudentEntity> {
+public interface StudentDAO  {
 
 }
 
